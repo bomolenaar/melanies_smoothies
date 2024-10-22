50753628877fb7f86ff4432bcef1a855fad9ac02
Bo Molenaar <45235011+bomolenaar@users.noreply.github.com>
2024-10-22 09:53:19 +0200

    Update streamlit_app.py
    added fruityvice requests (which don't work)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -31,6 +32,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """
                     insert into smoothies.public.orders(ingredients, name_on_order)
